5827cd0301bc5f7cc1e3371a706b263ce1e6dc12
Shivamkakade45 <shivamkakade95@gmail.com>
2025-01-22 20:13:40 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,19 @@
+public class Swapping
+
+{
+
+    public static void main(String x[])
+   
+     {
+        int a = 4, b= 5;
+        int a = c;
+        int a = b;
+         b = c;
+
+       System.out.printf("a:= %d",  +a);
+       System.out.printf("b:= %d",  +b);
+
+     }
+} 
+
+    
